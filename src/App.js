0465caeb59489dee760049327e0a658f11a5e4ec
Prashant Chaudhari <prashantchaudhari881@gmail.com>
2019-07-28 14:40:51 +0530

    convert createElement calls with JSX

--- src/App.js
+++ src/App.js
@@ -2,24 +2,12 @@ import React from "react";
 import Pet from "./Pet";
 
 function App() {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me"),
-    React.createElement(Pet, {
-      name: "Vegas",
-      animal: "Dog",
-      breed: "Golden Retriever"
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Parrot"
-    }),
-    React.createElement(Pet, {
-      name: "Medusa",
-      animal: "Bird",
-      breed: "Cobra"
-    })
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me</h1>
+      <Pet name="Vegas" animal="Dog" breed="Golden Retriever" />
+    </div>
+  );
 }
 
 export default App;
