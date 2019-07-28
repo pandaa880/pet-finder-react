85b339e9e851947b588466ae87796fd00ec49eca
Prashant Chaudhari <prashantchaudhari881@gmail.com>
2019-07-28 16:39:33 +0530

    replace html markup with custom hook based dropdown

---
+++ src/SearchParams.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+
+import useDropdown from "./UseDropdown";
+
+function SearchParams() {
+  const [location, setLocation] = useState("Seattle, WA");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            type="text"
+            name="location"
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={e => setLocation(e.target.value)}
+          />
+        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default SearchParams;
