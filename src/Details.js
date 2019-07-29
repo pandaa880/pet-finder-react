cf7d43f38e225d181ec0d336a925011725861b42
Prashant Chaudhari <prashantchaudhari881@gmail.com>
2019-07-29 12:45:55 +0530

    convert to class component and add carousel component

--- src/Details.js
+++ src/Details.js
@@ -1,14 +1,47 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
+import Carousel from "./Carousel";
 
-function Details(props) {
-  return (
-    <div>
-      <h1>Hi... LOL</h1>
-      <pre>
-        <code>{JSON.stringify(props, null, 4)}</code>
-      </pre>
-    </div>
-  );
+class Details extends React.Component {
+  state = {
+    loading: true
+  };
+
+  componentDidMount() {
+    const { id } = this.props;
+
+    pet.animal(id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false
+      });
+    });
+  }
+
+  render() {
+    const { loading, animal, breed, location, description, media, name } = this.state;
+
+    if (loading) {
+      return <h1>Loading ...</h1>;
+    }
+
+    return (
+      <div className="details">
+        <Carousel media={media} />
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <button type="button">Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Details;
